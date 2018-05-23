55e012a8d67a0d1002e2407f2bdda9dbbd28a671
iOSDevLog <iosdevlog@iosdevlog.com>
2018-05-23 11:06:59 +0800

    First Commit!
    Hello, World!

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("hello, world");
+    }
+}
